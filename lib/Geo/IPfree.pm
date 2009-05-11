b2f2ed3fd0cfeb94d3a318e7c26637bc92be48aa
Brian Cassidy <bricas@cpan.org>
2009-05-11 11:11:28 -0300

    tidy up baseX array

--- lib/Geo/IPfree.pm
+++ lib/Geo/IPfree.pm
@@ -15,8 +15,7 @@ our @EXPORT = qw(LookUp LoadDB);
 our @EXPORT_OK = @EXPORT;
 
 my $def_db = 'ipscountry.dat';
-
-my @baseX = ( qw(0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z .), ',', qw(; ' " ` < > { } [ ] = + - ~ * @), '#', qw(% $ & ! ?) );
+my @baseX  = ( 0..9, 'A'..'Z', 'a'..'z', split( m{}, q(.,;'"`<>{}[]=+-~*@#%$&!?) ) );
 
 my %countrys = ( qw(
 -- N/A L0 localhost I0 IntraNet A1 Anonymous_Proxy A2 Satellite_Provider
