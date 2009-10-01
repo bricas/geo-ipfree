8b7399e3afcf71b4e45c4a8c66b4b9870ade27ae
Brian Cassidy <bricas@cpan.org>
2009-10-01 10:55:21 -0300

    data cleanup

--- lib/Geo/IPfree.pm
+++ lib/Geo/IPfree.pm
@@ -14,65 +14,29 @@ our $VERSION = '0.7';
 our @EXPORT    = qw(LookUp LoadDB);
 our @EXPORT_OK = @EXPORT;
 
-my $def_db = 'ipscountry.dat';
-my @baseX  = (
+my $DEFAULT_DB = 'ipscountry.dat';
+my $cache_expire = 1000;
+my @baseX      = (
     0 .. 9,
     'A' .. 'Z',
     'a' .. 'z',
     split( m{}, q(.,;'"`<>{}[]=+-~*@#%$&!?) )
 );
 
-my %countrys = (
-    qw(
-        -- N/A L0 localhost I0 IntraNet A1 Anonymous_Proxy A2 Satellite_Provider
-        AD Andorra AE United_Arab_Emirates AF Afghanistan AG Antigua_and_Barbuda AI Anguilla AL Albania AM Armenia AN Netherlands_Antilles
-        AO Angola AP Asia/Pacific_Region AQ Antarctica AR Argentina AS American_Samoa AT Austria AU Australia AW Aruba AZ Azerbaijan BA Bosnia_and_Herzegovina BB Barbados BD Bangladesh
-        BE Belgium BF Burkina_Faso BG Bulgaria BH Bahrain BI Burundi BJ Benin BM Bermuda BN Brunei_Darussalam BO Bolivia BR Brazil BS Bahamas BT Bhutan BV Bouvet_Island BW Botswana
-        BY Belarus BZ Belize CA Canada CC Cocos_(Keeling)_Islands CD),
-    'Congo,_The_Democratic_Republic_of_the',
-    qw(CF Central_African_Republic CG Congo CH Switzerland CI Cote_D'Ivoire CK Cook_Islands
-        CL Chile CM Cameroon CN China CO Colombia CR Costa_Rica CU Cuba CV Cape_Verde CX Christmas_Island CY Cyprus CZ Czech_Republic DE Germany DJ Djibouti DK Denmark DM Dominica
-        DO Dominican_Republic DZ Algeria EC Ecuador EE Estonia EG Egypt EH Western_Sahara ER Eritrea ES Spain ET Ethiopia EU Europe FI Finland FJ Fiji FK Falkland_Islands_(Malvinas)
-        FM), 'Micronesia,_Federated_States_of',
-    qw(FO Faroe_Islands FR France FX), 'France,_Metropolitan',
-    qw(GA Gabon GB United_Kingdom GD Grenada GE Georgia GF French_Guiana GH Ghana GI Gibraltar
-        GL Greenland GM Gambia GN Guinea GP Guadeloupe GQ Equatorial_Guinea GR Greece GS South_Georgia_and_the_South_Sandwich_Islands GT Guatemala GU Guam GW Guinea-Bissau GY Guyana
-        HK Hong_Kong HM Heard_Island_and_McDonald_Islands HN Honduras HR Croatia HT Haiti HU Hungary ID Indonesia IE Ireland IL Israel IN India IO British_Indian_Ocean_Territory
-        IQ Iraq IR), 'Iran,_Islamic_Republic_of',
-    qw(IS Iceland IT Italy JM Jamaica JO Jordan JP Japan KE Kenya KG Kyrgyzstan KH Cambodia KI Kiribati KM Comoros KN Saint_Kitts_and_Nevis
-        KP), "Korea,_Democratic_People's_Republic_of", 'KR',
-    'Korea,_Republic_of',
-    qw(KW Kuwait KY Cayman_Islands KZ Kazakhstan LA Lao_People's_Democratic_Republic LB Lebanon LC Saint_Lucia LI Liechtenstein
-        LK Sri_Lanka LR Liberia LS Lesotho LT Lithuania LU Luxembourg LV Latvia LY Libyan_Arab_Jamahiriya MA Morocco MC Monaco MD),
-    'Moldova,_Republic_of', qw(MG Madagascar MH Marshall_Islands
-        MK), 'Macedonia,_the_Former_Yugoslav_Republic_of',
-    qw(ML Mali MM Myanmar MN Mongolia MO Macau MP Northern_Mariana_Islands MQ Martinique MR Mauritania MS Montserrat MT Malta MU Mauritius
-        MV Maldives MW Malawi MX Mexico MY Malaysia MZ Mozambique NA Namibia NC New_Caledonia NE Niger NF Norfolk_Island NG Nigeria NI Nicaragua NL Netherlands NO Norway NP Nepal
-        NR Nauru NU Niue NZ New_Zealand OM Oman PA Panama PE Peru PF French_Polynesia PG Papua_New_Guinea PH Philippines PK Pakistan PL Poland PM Saint_Pierre_and_Miquelon PN Pitcairn
-        PR Puerto_Rico PS), 'Palestinian_Territory,_Occupied',
-    qw(PT Portugal PW Palau PY Paraguay QA Qatar RE Reunion RO Romania RU Russian_Federation RW Rwanda SA Saudi_Arabia SB Solomon_Islands
-        SC Seychelles SD Sudan SE Sweden SG Singapore SH Saint_Helena SI Slovenia SJ Svalbard_and_Jan_Mayen SK Slovakia SL Sierra_Leone SM San_Marino SN Senegal SO Somalia SR Suriname
-        ST Sao_Tome_and_Principe SV El_Salvador SY Syrian_Arab_Republic SZ Swaziland TC Turks_and_Caicos_Islands TD Chad TF French_Southern_Territories TG Togo TH Thailand TJ Tajikistan
-        TK Tokelau TM Turkmenistan TN Tunisia TO Tonga TP East_Timor TR Turkey TT Trinidad_and_Tobago TV Tuvalu TW),
-    'Taiwan,_Province_of_China', 'TZ', 'Tanzania,_United_Republic_of',
-    qw(UA Ukraine
-        UG Uganda UM United_States_Minor_Outlying_Islands US United_States UY Uruguay UZ Uzbekistan VA Holy_See_(Vatican_City_State) VC Saint_Vincent_and_the_Grenadines VE Venezuela
-        VG), 'Virgin_Islands,_British', 'VI', 'Virgin_Islands,_U.S.',
-    qw(VN Vietnam VU Vanuatu WF Wallis_and_Futuna WS Samoa YE Yemen YT Mayotte YU Yugoslavia ZA South_Africa ZM Zambia ZR Zaire ZW Zimbabwe
-        )
-);
+my ( %baseX, $base, $THIS, %countrys );
 
-my ( %baseX, $base, $THIS );
-
-my $cache_expire = 1000;
-
-# DECLARE BASE LIB
 {
     my $c = 0;
     %baseX = map { $_ => ( $c++ ) } @baseX;
     $base = @baseX;
 
-    foreach my $Key ( keys %countrys ) { $countrys{ $Key } =~ s/_/ /gs; }
+    my @data;
+    while( <DATA> ) {
+        last if m{^__END__};
+        chomp;
+        push @data, split m{ }, $_, 2;
+    }
+    %countrys = @data;
 }
 
 sub new {
@@ -101,7 +65,7 @@ sub LoadDB {
     my $this = shift;
     my ( $db_file ) = @_;
 
-    if ( -d $db_file ) { $db_file .= "/$def_db"; }
+    if ( -d $db_file ) { $db_file .= "/$DEFAULT_DB"; }
 
     if ( !-s $db_file ) {
         Carp::croak( "Can't load database, blank or not there: $db_file" );
@@ -254,7 +218,7 @@ sub find_db_file {
     my ( $lib ) = ( $INC{ 'Geo/IPfree.pm' } =~ /^(.*?)[\\\/]+[^\\\/]+$/gs );
     push @locations, $lib;
 
-    for my $file ( map { "$_/$def_db" } @locations ) {
+    for my $file ( map { "$_/$DEFAULT_DB" } @locations ) {
         return $file if -e $file;
     }
 }
@@ -334,6 +298,255 @@ sub baseX2dec {
 
 1;
 
+__DATA__
+-- N/A
+L0 localhost
+I0 IntraNet
+A1 Anonymous Proxy
+A2 Satellite Provider
+AD Andorra
+AE United Arab Emirates
+AF Afghanistan
+AG Antigua and Barbuda
+AI Anguilla
+AL Albania
+AM Armenia
+AN Netherlands Antilles
+AO Angola
+AP Asia/Pacific Region
+AQ Antarctica
+AR Argentina
+AS American Samoa
+AT Austria
+AU Australia
+AW Aruba
+AZ Azerbaijan
+BA Bosnia and Herzegovina
+BB Barbados
+BD Bangladesh
+BE Belgium
+BF Burkina Faso
+BG Bulgaria
+BH Bahrain
+BI Burundi
+BJ Benin
+BM Bermuda
+BN Brunei Darussalam
+BO Bolivia
+BR Brazil
+BS Bahamas
+BT Bhutan
+BV Bouvet Island
+BW Botswana
+BY Belarus
+BZ Belize
+CA Canada
+CC Cocos (Keeling) Islands
+CD Congo, The Democratic Republic of the
+CF Central African Republic
+CG Congo
+CH Switzerland
+CI Cote D'Ivoire
+CK Cook Islands
+CL Chile
+CM Cameroon
+CN China
+CO Colombia
+CR Costa Rica
+CU Cuba
+CV Cape Verde
+CX Christmas Island
+CY Cyprus
+CZ Czech Republic
+DE Germany
+DJ Djibouti
+DK Denmark
+DM Dominica
+DO Dominican Republic
+DZ Algeria
+EC Ecuador
+EE Estonia
+EG Egypt
+EH Western Sahara
+ER Eritrea
+ES Spain
+ET Ethiopia
+EU Europe
+FI Finland
+FJ Fiji
+FK Falkland Islands (Malvinas)
+FM Micronesia, Federated States of
+FO Faroe Islands
+FR France
+FX France Metropolitan
+GA Gabon
+GB United Kingdom
+GD Grenada
+GE Georgia
+GF French Guiana
+GH Ghana
+GI Gibraltar
+GL Greenland
+GM Gambia
+GN Guinea
+GP Guadeloupe
+GQ Equatorial Guinea
+GR Greece
+GS South Georgia and the South Sandwich Islands
+GT Guatemala
+GU Guam
+GW Guinea-Bissau
+GY Guyana
+HK Hong Kong
+HM Heard Island and McDonald Islands
+HN Honduras
+HR Croatia
+HT Haiti
+HU Hungary
+ID Indonesia
+IE Ireland
+IL Israel
+IN India
+IO British Indian Ocean Territory
+IQ Iraq
+IR Iran, Islamic Republic of
+IS Iceland
+IT Italy
+JM Jamaica
+JO Jordan
+JP Japan
+KE Kenya
+KG Kyrgyzstan
+KH Cambodia
+KI Kiribati
+KM Comoros
+KN Saint Kitts and Nevis
+KP Korea, Democratic People's Republic of
+KR Korea, Republic of
+KW Kuwait
+KY Cayman Islands
+KZ Kazakhstan
+LA Lao People's Democratic Republic
+LB Lebanon
+LC Saint Lucia
+LI Liechtenstein
+LK Sri Lanka
+LR Liberia
+LS Lesotho
+LT Lithuania
+LU Luxembourg
+LV Latvia
+LY Libyan Arab Jamahiriya
+MA Morocco
+MC Monaco
+MD Moldova, Republic of
+MG Madagascar
+MH Marshall Islands
+MK Macedonia, the Former Yugoslav Republic of
+ML Mali
+MM Myanmar
+MN Mongolia
+MO Macau
+MP Northern Mariana Islands
+MQ Martinique
+MR Mauritania
+MS Montserrat
+MT Malta
+MU Mauritius
+MV Maldives
+MW Malawi
+MX Mexico
+MY Malaysia
+MZ Mozambique
+NA Namibia
+NC New Caledonia
+NE Niger
+NF Norfolk Island
+NG Nigeria
+NI Nicaragua
+NL Netherlands
+NO Norway
+NP Nepal
+NR Nauru
+NU Niue
+NZ New Zealand
+OM Oman
+PA Panama
+PE Peru
+PF French Polynesia
+PG Papua New Guinea
+PH Philippines
+PK Pakistan
+PL Poland
+PM Saint Pierre and Miquelon
+PN Pitcairn
+PR Puerto Rico
+PS Palestinian Territory, Occupied
+PT Portugal
+PW Palau
+PY Paraguay
+QA Qatar
+RE Reunion
+RO Romania
+RU Russian Federation
+RW Rwanda
+SA Saudi Arabia
+SB SolomonIslands
+SC Seychelles
+SD Sudan
+SE Sweden
+SG Singapore
+SH Saint Helena
+SI Slovenia
+SJ Svalbard and Jan Mayen
+SK Slovakia
+SL Sierra Leone
+SM San Marino
+SN Senegal
+SO Somalia
+SR Suriname
+ST Sao Tome and Principe
+SV El Salvador
+SY Syrian Arab Republic
+SZ Swaziland
+TC Turks and Caicos Islands
+TD Chad
+TF French Southern Territories
+TG Togo
+TH Thailand
+TJ Tajikistan
+TK Tokelau
+TM Turkmenistan
+TN Tunisia
+TO Tonga
+TP East Timor
+TR Turkey
+TT Trinidad and Tobago
+TV Tuvalu
+TW Taiwan, Province of China
+TZ Tanzania, United Republic of
+UA Ukraine
+UG Uganda
+UM United States Minor Outlying Islands
+US United States
+UY Uruguay
+UZ Uzbekistan
+VA Holy See (Vatican City State)
+VC Saint Vincent and the Grenadines
+VE Venezuela
+VG Virgin Islands, British
+VI Virgin Islands, U.S.
+VN Vietnam
+VU Vanuatu
+WF Wallis and Futuna
+WS Samoa
+YE Yemen
+YT Mayotte
+YU Yugoslavia
+ZA South Africa
+ZM Zambia
+ZR Zaire
+ZW Zimbabwe
 __END__
 
 =head1 NAME
