b9dd81b00d141c6f973270ba3aed01954e3b77a2
Brian Cassidy <bricas@cpan.org>
2013-06-14 09:39:22 -0300

    Updated database: Fri Jun 14 06:40:02 2013 UTC.

--- lib/Geo/IPfree.pm
+++ lib/Geo/IPfree.pm
@@ -9,7 +9,7 @@ use Carp qw();
 require Exporter;
 our @ISA = qw(Exporter);
 
-our $VERSION = '1.130450';
+our $VERSION = '1.131650';
 
 our @EXPORT    = qw(LookUp LoadDB);
 our @EXPORT_OK = @EXPORT;
